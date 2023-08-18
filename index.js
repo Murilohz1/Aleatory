c6a9dd7894862f7f0b98e9d16ea398593b7bb771
Murilohz1 <141884877+Murilohz1@users.noreply.github.com>
2023-08-18 14:24:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const { spawn } = require('child_process');
 
-spawn('bash', ['-c', 'cd aleatory-bot && sh start.sh'], { stdio: 'inherit' });
+spawn('bash', ['-c', 'cd aleatory-bot && node iniciar.js'], { stdio: 'inherit' });
